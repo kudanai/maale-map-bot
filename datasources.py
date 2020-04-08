c1cf7555f76eded6d18ccdfb2ab76cb8dd51a6c2
kudanai <kudanai@gmail.com>
2020-04-08 19:26:08 +0800

    change data source to updated thing, update start/help commands

--- datasources.py
+++ datasources.py
@@ -2,7 +2,7 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 import hashlib
 import sqlite3
-from aiogram.types import InlineQuery, InlineQueryResultVenue
+from aiogram.types import InlineQuery, InlineQueryResultVenue, Venue
 
 
 class AddressSource():
@@ -12,6 +12,12 @@ class AddressSource():
     async def get_addresses_results(self, query):
         """
         must return an array of InlineQueryResultVenu
+        """
+        pass
+
+    async def get_address(self, query):
+        """
+        must return an address dict
         """
         pass
 
@@ -28,27 +34,48 @@ class SQLiteDatasource(AddressSource):
 
     Uses fts5 for search: 
     note: update the fts table if data is changed
-        CREATE VIRTUAL TABLE IF NOT EXISTS addresses_index USING fts5(CATEGORY, DISTRICT, LAT, LNG, NAME, POSTCODE, ROAD, tokenize=porter);
+        CREATE VIRTUAL TABLE IF NOT EXISTS addresses_index USING fts5(name, district, lat, lng, road, postalcode, tokenize=porter);
         DELETE FROM addresses_index;
-        INSERT INTO addresses_index SELECT * FROM addresses;
+        INSERT INTO addresses_index SELECT name, district, lat, lng, road, postalcode FROM addresses;
     """
 
     def __init__(self, db_source):
         self.__dbconn = sqlite3.connect(db_source)
+
+    async def get_address(self, query):
+        """
+        must return a reply
+        """
+        reply = None
+        if query:
+            c = self.__dbconn.cursor()
+            res = c.execute("SELECT * FROM addresses_index WHERE addresses_index MATCH ? ORDER BY rank LIMIT 1;", (f"name:{query}",))
+            for row in res:
+                reply = {
+                    "id": self._get_address_hash(row[0]),
+                    "latitude": row[2],
+                    "longitude": row[3],
+                    "title": str(row[0]).title(),
+                    "address": f"{row[4]}, {row[5]}".title()
+                }
+
+            c.close()
+
+        return reply
 
     async def get_addresses_results(self, query):
         items = []
         
         if query:
             c = self.__dbconn.cursor()
-            res = c.execute("SELECT * FROM addresses_index WHERE addresses_index MATCH ? ORDER BY rank LIMIT 10;", (f"NAME:{query}*",))
+            res = c.execute("SELECT * FROM addresses_index WHERE addresses_index MATCH ? ORDER BY rank LIMIT 10;", (f"name:{query}*",))
             for row in res:
                 loc = InlineQueryResultVenue(
-                        id = self._get_address_hash(row[4]),
+                        id = self._get_address_hash(row[0]),
                         latitude=row[2],
                         longitude=row[3],
-                        title=str(row[4]).capitalize(),
-                        address=f"{row[6]}, {row[5]}".capitalize()
+                        title=str(row[0]).title(),
+                        address=f"{row[4]}, {row[5]}".title()
                     )
                 items.append(loc)
 
@@ -80,6 +107,12 @@ class PandasDatasource(AddressSource):
 
     def __query(self,q):
         return self.__df[self.__df.apply(self.__get_filter(q), axis=1) > 70]
+
+    async def get_address(self, query):
+        """
+        must return an array of InlineQueryResultVenu
+        """
+        pass
 
     async def get_addresses_results(self, query):
         items = []
--- map_bot.py
+++ map_bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor
-from aiogram.types import InlineQuery, InlineQueryResultVenue
+from aiogram.types import InlineQuery, InlineQueryResultVenue, Message
 from datasources import AddressSource, SQLiteDatasource, PandasDatasource
 from settings import MAPBOT_API_TOKEN, USE_CSV, DEBUG, SQLITE_SOURCE, CSV_SOURCE
 
@@ -10,10 +10,33 @@ dp = Dispatcher(bot)
 data_source:AddressSource = PandasDatasource(CSV_SOURCE) if USE_CSV else SQLiteDatasource(SQLITE_SOURCE)
 
 @dp.inline_handler()
-async def address_query_handler(inline_query: InlineQuery):
+async def inline_query_handler(inline_query: InlineQuery):
     q = inline_query.query
     results = await data_source.get_addresses_results(q)
     await bot.answer_inline_query(inline_query.id, results=results, cache_time=1)
+
+
+@dp.message_handler(commands=["start", "help"])
+async def start_handler(message: Message):
+    await message.answer(
+        "Welcome to Male' Maps Bot. Type in the address you wish to search. You can also query this bot inline"
+    )
+
+
+@dp.message_handler()
+async def message_handler(message: Message):
+    address = await data_source.get_address(message.text)
+    if address:
+        await message.answer_venue(
+            address["latitude"],
+            address["longitude"],
+            address["title"],
+            address["address"]
+        )
+    else:
+        await message.answer(
+            "Sorry! Could not find the address"
+        )
 
 
 if __name__ == '__main__':
