d0baa6d2653440322484f260d814f8d22380025a
kudanai <kudanai@gmail.com>
2020-04-09 17:50:31 +0800

    try catch hell

--- map_bot.py
+++ map_bot.py
@@ -26,30 +26,35 @@ async def inline_query_handler(inline_query: InlineQuery):
 
 @dp.message_handler(commands=["start", "help"])
 async def start_handler(message: Message):
-    await message.answer(
-        "Welcome to Male' Maps Bot. Type in the address you wish to search. You can also query this bot inline"
-    )
+    try:
+        await message.answer(
+            "Welcome to Male' Maps Bot. Type in the address you wish to search. You can also query this bot inline"
+        )
+    except Exception as e:
+        pass
 
 
 @dp.message_handler()
 async def message_handler(message: Message):
-    addresses = await data_source.get_addresses_results(message.text)
-    address = addresses[0] if addresses else None   # pick one
-    if address:
-        await message.answer_venue(
-            address["latitude"],
-            address["longitude"],
-            address["title"],
-            address["address"]
-        )
-    else:
-        suggestions = await data_source.get_suggestions(message.text)
-        suggestions_flat = "\n".join([f"* {x}" for x in suggestions])
-        await message.answer(
-            "Sorry! Could not find the address\n" + 
-            ("Did you mean:\n\n" + f"{suggestions_flat}" if suggestions else "")
-        )
-
+    try:
+        addresses = await data_source.get_addresses_results(message.text)
+        address = addresses[0] if addresses else None   # pick one
+        if address:
+            await message.answer_venue(
+                address["latitude"],
+                address["longitude"],
+                address["title"],
+                address["address"]
+            )
+        else:
+            suggestions = await data_source.get_suggestions(message.text)
+            suggestions_flat = "\n".join([f"* {x}" for x in suggestions])
+            await message.answer(
+                "Sorry! Could not find the address\n" + 
+                ("Did you mean:\n\n" + f"{suggestions_flat}" if suggestions else "")
+            )
+    except Exception as e:
+        pass
 
 async def on_startup(dp):
     await bot.set_webhook(f"{WEBHOOK_URL}{WEBHOOK_PATH}")
@@ -66,6 +71,7 @@ if __name__ == '__main__':
             WEBHOOK_PATH, 
             on_startup=on_startup, 
             on_shutdown=on_shutdown, 
+            skip_updates=True,
             host=WEBAPP_HOST,
             port=WEBAPP_PORT)
     else: 
